fec6fabe1e48fe92f202864b051e02775e560bee
Tranas <Tranas@gmail.com>
2013-01-25 23:49:55 -0800

    Added a bunch of wild pickles of fuckassery.

--- heartPump/Assets/Code/Controller.cs
+++ heartPump/Assets/Code/Controller.cs
@@ -3,41 +3,83 @@ using System.Collections;
 
 public class Controller : MonoBehaviour {
 
+    public enum State
+    {
+        Dead,
+        Alive
+    };
 
     //Calvin Harris 18 months
+
+    public State myState;
+
     private tk2dBaseSprite theSprite;
     private string myID;
     private float speed = 3.0f;
+    
+    const float RIGHT_X_CONSTRAINT = 1.2f;
+    const float LEFT_X_CONSTRAINT = -1.2f;
+    const float UP_Y_CONSTRAINT = 1.8f;
+    const float DOWN_Y_CONSTRAINT = 0.2f;
     
 
 	// Use this for initialization
 	void Start () 
     {
-        theSprite = this.GetComponent<tk2dBaseSprite>();
+        myState = State.Alive;
+        theSprite = this.gameObject.GetComponent<tk2dSprite>();
         myID = this.gameObject.name;
 	}
 	
 	// Update is called once per frame
 	void Update () 
     {
-        float translateX = Input.GetAxis(myID + ": Horizontal") * speed * Time.deltaTime;
-        float translateY = Input.GetAxis(myID + ": Vertical") * speed * Time.deltaTime;
-
-        transform.Translate(translateX, translateY, 0);
-
-        if (Input.GetButtonDown(myID + ": A Button"))
+        if (myState == State.Alive)
         {
-            theSprite.spriteId = 2;
-        }
+            float translateX = Input.GetAxis(myID + ": Horizontal") * speed * Time.deltaTime;
+            float translateY = Input.GetAxis(myID + ": Vertical") * speed * Time.deltaTime;
 
-        else if (Input.GetButtonDown(myID + ": B Button"))
-        {
-            theSprite.spriteId = 1;
+            transform.Translate(translateX, translateY, 0);
+
+            if (transform.position.x >= RIGHT_X_CONSTRAINT)
+            {
+                transform.position = new Vector3(RIGHT_X_CONSTRAINT, transform.position.y, transform.position.z);
+            }
+
+            if (transform.position.x <= LEFT_X_CONSTRAINT)
+            {
+                transform.position = new Vector3(LEFT_X_CONSTRAINT, transform.position.y, transform.position.z);
+            }
+
+            if (transform.position.y >= UP_Y_CONSTRAINT)
+            {
+                transform.position = new Vector3(transform.position.x, UP_Y_CONSTRAINT, transform.position.z);
+            }
+
+            if (transform.position.y <= DOWN_Y_CONSTRAINT)
+            {
+                transform.position = new Vector3(transform.position.x, DOWN_Y_CONSTRAINT, transform.position.z);
+            }
+
+            if (Input.GetButtonDown(myID + ": A Button"))
+            {
+                theSprite.spriteId = 2;
+            }
+
+            else if (Input.GetButtonDown(myID + ": B Button"))
+            {
+                theSprite.spriteId = 1;
+            }
+
+            else
+            {
+                theSprite.spriteId = 0;
+            }
         }
 
         else
         {
-            theSprite.spriteId = 0;
+            Debug.Log(myID + " is Dead.");
         }
 	}
 }
